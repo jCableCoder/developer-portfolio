140d307a62329e6c6069bdbb81f87adf738243e2
jCableCoder <joshcable215@gmail.com>
2024-11-25 00:10:53 -0500

    Updated Projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,7 +1,7 @@
 import one from '../assets/svg/projects/Weather Plus App.png'
 import two from '../assets/svg/projects/Band.png'
-import three from '../assets/svg/projects/Cavs Intel.png'
-import four from '../assets/svg/projects/four.svg'
+import three from '../assets/svg/projects/TekScale.png'
+import four from '../assets/svg/projects/Cavs Intel.png'
 import five from '../assets/svg/projects/five.svg'
 import six from '../assets/svg/projects/six.svg'
 import seven from '../assets/svg/projects/seven.svg'
@@ -29,58 +29,58 @@ export const projectsData = [
     },
     {
         id: 3,
-        projectName: 'Cavs Intel',
-        projectDesc: "I created Cavs Intel in 2023, and it's been a real labor of love. Go Cavs!", 
-        tags: ['WordPress, Canva, Content Creation'],
-        // code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://cavsintel.com',
+        projectName: 'TekScale',
+        projectDesc: "This is a portfolio project I created for a fictitious company called TekScale. It's a simple, responsive site that showcases the company's services.",
+        tags: ['Vanilla JS, HTML, CSS'],
+        code: 'https://github.com/jCableCoder/developer-portfolio',
+        demo: 'https://tekscale.netlify.app/',
         image: three
     },
     {
         id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Cavs Intel',
+        projectDesc: "I created Cavs Intel in 2023, and it's been a real labor of love. Go Cavs!",
+        tags: ['WordPress, Canva, Content Creation'],
+        code: '',
+        demo: 'https://cavsintel.com/',
         image: four
     },
-    {
-        id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: six
-    },
-    {
-        id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: seven
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: eight
-    },
+    // {
+    //     id: 5,
+    //     projectName: 'E-Commerce App',
+    //     projectDesc: 'A Simple E-commerce application',
+    //     tags: ['React Native', 'Firebase'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: five
+    // },
+    // {
+    //     id: 6,
+    //     projectName: 'Uber Lite',
+    //     projectDesc: 'Uber clone',
+    //     tags: ['Flutter'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: six
+    // },
+    // {
+    //     id: 7,
+    //     projectName: 'Stock Market App',
+    //     projectDesc: 'A simple stock market API app',
+    //     tags: ['React', 'Redux', 'Bootstrap'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: seven
+    // },
+    // {
+    //     id: 8,
+    //     projectName: 'Car Pooling System',
+    //     projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
+    //     tags: ['Flutter', 'React'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: eight
+    // },
 ]
 
 
